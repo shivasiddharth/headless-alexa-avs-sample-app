924eb901989782e4d2ce08f8af34f89e4b2c6d68
shivasiddharth <ushivasiddharth@gmail.com>
2017-10-18 22:11:46 +0100

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-read -r -p "Enter the location of alexa-avs-sample-app folder, Eg: if it is in desktop then enter ~Desktop or \nif it is in /home/pi/ directory, then enter /home/pi" path
-read -r -p "Have you placed the alexa app in $path? (Y/N)" verify_interrupt
+read -r -p "Have you checked the Alexa App location specified in the scripts? (Y/N)" verify_interrupt
 case ${verify_interrupt} in
             [yY] )
                 echo "Continuing ..."
@@ -10,9 +9,6 @@ case ${verify_interrupt} in
                 exit
             ;;
 esac
-sed '1 a cd $path/alexa-avs-sample-app/samples' /home/pi/headless-alexa-avs-sample-app/scripts/clientstart.sh
-sed '1 a cd $path/alexa-avs-sample-app/samples' /home/pi/headless-alexa-avs-sample-app/scripts/companionstart.sh
-sed '1 a cd $path/alexa-avs-sample-app/samples' /home/pi/headless-alexa-avs-sample-app/scripts/wakeword.sh
 sudo chmod +x /home/pi/headless-alexa-avs-sample-app/scripts/clientstart.sh
 sudo chmod +x /home/pi/headless-alexa-avs-sample-app/scripts/companionstart.sh
 sudo chmod +x /home/pi/headless-alexa-avs-sample-app/scripts/service-installer.sh
